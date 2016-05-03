f5090045829b179dc622b1584029486b3db3029c
Edgar Forero <edgar@toro-labs.com>
2016-05-03 16:46:56 -0500

    test external calls

--- app/controllers/external.php
+++ app/controllers/external.php
@@ -28,6 +28,23 @@ switch($verb) {
         $data = $e_data['events'];
         break;
     
+    case 'close_events1':
+        //$ipAddress = Utility::get_ip_address();
+        $ipAddress = "38.105.128.254";
+        $ipurl  = 'http://api.ipinfodb.com/v3/ip-city/?key=e12e8a9321c609d76b8c956020e4dd2d0f8f2961357f41ec6fda74e8905b7547&ip=' . $ipAddress . '&format=json';
+        $ipdata = json_decode(file_get_contents($ipurl), true);
+        $cs     = $ipdata['cityName'] . ', ' . $ipdata['regionName'];
+        $query  = array('city_state'    => $cs,
+                      'category_id'     => Config::te_categoryid(),
+                      'within'          => 25,
+                      'page'            => 1,
+                      'per_page'        => 8,
+                      'occurs_at.gte'   => $_GET["actual_date"],
+                      'order_by'        => 'events.occurs_at ASC, events.popularity_score DESC');
+        $e_data = $teClient->listEvents($query);
+        $data = $e_data['events'];
+        break;
+    
     case 'performer_events':
         if($_GET["home_only_games"] == 'true'){
             $query  = array('performer_id'      => $_GET["id"],
