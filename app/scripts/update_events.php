5a4363869f3c5ea6124234fa4990e1948910b62f
Fabian Santos <fabian@toro-labs.com>
2016-05-27 17:06:46 -0500

    second test

---
+++ app/scripts/update_events.php
@@ -0,0 +1,55 @@
+<?php 
+	require_once 'app/classes/event.php';
+	
+	use \TicketEvolution\Client as TEvoClient;
+
+	$teClient = new TEvoClient(['baseUrl'    => Config::te_url(),
+	                            'apiVersion' => Config::te_version(),
+	                            'apiToken'   => Config::te_api_token(),
+	                            'apiSecret'  => Config::te_api_secret()]);
+
+	switch($verb) {
+	$query  = array(//'performer_id'      => 16425,//$_GET["id"],
+        'category_id'       => Config::te_categoryid(),
+        'page'              => 1,//(int)$_GET["page"],
+        'per_page'          => 100000,//(int)$_GET["per_page"],
+        //'occurs_at.gte'     => $_GET["actual_date"],
+        //'order_by'          => 'events.occurs_at ASC, events.popularity_score DESC');
+        );
+    $e_data = $teClient->listEvents($query);
+    $data = [];
+    foreach($e_data['events'] AS $tevo_event) {
+        $event = new Event;
+        $test = $event->te_to_gh($tevo_event["id"]);
+        if($test == false){
+            $date = date('Y-m-d H:i:s', strtotime(str_replace('-', '/', $tevo_event["occurs_at"])));
+            if(count($tevo_event["performances"]) == 2){
+                if($tevo_event["performances"][0]["primary"] == true){
+                    $eventData = array('te_uid' => $tevo_event["id"],
+                    'te_performer_home_id' => $tevo_event["performances"][0]["performer"]["id"],
+                    'te_performer_visit_id' => $tevo_event["performances"][1]["performer"]["id"],
+                    'data_event' => json_encode($tevo_event),
+                    'te_date' => $date,
+                    );
+                }
+                else{
+                    $eventData = array('te_uid' => $tevo_event["id"],
+                    'te_performer_home_id' => $tevo_event["performances"][1]["performer"]["id"],
+                    'te_performer_visit_id' => $tevo_event["performances"][0]["performer"]["id"],
+                    'data_event' => json_encode($tevo_event),
+                    'te_date' => $date,
+                    );
+                }
+            }
+            else{
+                $eventData = array('te_uid' => $tevo_event["id"],
+                'te_performer_home_id' => $tevo_event["performances"][0]["performer"]["id"],
+                'data_event' => json_encode($tevo_event),
+                'te_date' => $date,
+                );
+            }
+            $eventId = $event->add($eventData);
+        }
+    }
+    print "Current time: " . time();
+?>
