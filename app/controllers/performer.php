a980a699cffe35ee4f8a377c675cd8ab7834f77d
Fabian Santos <fabian@toro-labs.com>
2016-06-01 17:29:35 -0500

    fixed header

--- app/controllers/performer.php
+++ app/controllers/performer.php
@@ -23,7 +23,7 @@ $smarty->assign('hscripts', '');
 $header = $smarty->fetch('shared/header.tpl');
 $menu = $smarty->fetch('shared/menu.tpl');
 // Handle Footer
-$smarty->assign('fscripts', '<script src="/assets/js/moment.js"></script><script src="/assets/js/performer.js?v111"></script>');
+$smarty->assign('fscripts', '<script src="/assets/js/moment.js"></script><script src="/assets/js/performer.js?v112"></script>');
 $footer = $smarty->fetch('shared/footer.tpl');
 //$p_data = $teClient->showPerformer(['performer_id' => (int)$id]);
 $performer = new Performer;
