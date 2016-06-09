340b5160b46cc082db1f6a3b1b7d6f82710c942b
Fabian Santos <fabian@toro-labs.com>
2016-06-09 17:18:20 -0500

    small fixes

--- app/controllers/ticket.php
+++ app/controllers/ticket.php
@@ -242,7 +242,7 @@ case '';
 							CSSClass: "TicketRow",
 							Formatter: function(RowData, Key, CellValue, FormattedSection) {
 								var Code = "<div>";
-                                Code += "<div class=\"ticket-cell seat-link\"><button class =\"button green\" type=\"button\" onclick=\"BuyNow(\'" + RowData.id + "\',\'" + RowData.price + "\');\">" + RowData.price + "ea</button></div>";
+                                Code += "<div class=\"ticket-cell seat-link\"><button style=\"text-transform: none;\" class =\"button green\" type=\"button\" onclick=\"BuyNow(\'" + RowData.id + "\',\'" + RowData.price + "\');\">" + RowData.price + "ea</button></div>";
                                 Code += "<div class=\"ticket-cell guarantee guarantee-mobile\">Good Game<br />Guarantee&trade;</div>";
 								Code += "</div>";
 								return Code;
