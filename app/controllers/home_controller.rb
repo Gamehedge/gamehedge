3f2d9e9e67965bb787f7b21ebc9659675ee82bfb
wencelf <fabian@toro-labs.com>
2016-07-15 18:56:53 -0500

    fix

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,18 +3,7 @@ class HomeController < ApplicationController
   def index
   	
 	# Update dates for events
-	@connection = TicketEvolution::Connection.new({
-		  	:token => '5bfd4b6110681d224a8c1fa6333f375f',       # => (required) The API token, used to identify you
-		  	:secret => 'g3iR2RLeuzQA9vhDGfw5hRtGMnMDsimyOfQAJ4bi',      # => (required) The API secret, used to sign requests
-		                      #               More info: [http://developer.ticketevolution.com/signature_tool](http://developer.ticketevolution.com/signature_tool))
-		  	:version => 9,      # => (required) API version to use - the correct version at the time of this writing is 9
-		  	:mode => :production,  # => (optional) Specifies the server environment to use Valid options: :production or :sandbox
-		  	:logger => nil      # => (optional) Object to use for logging requests and
-		                      #               responses. Any 'Logger' instance object
-		                      #               is valid. EX: Logger.new('log/te_api.log')
-		})
-		@order = @connection.orders.show(3075490)
-		render json: @order
+	
 	#render json: Php.unserialize(Order.where(te_order_id: 3074944).first.order_data) #=> {"foo"=>"bar"}
 	# @events = Order.all
 	# @events.each do |event|
