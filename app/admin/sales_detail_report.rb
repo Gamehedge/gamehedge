287a9a18b8a1b22cbce41c79577299f3b007b405
wencelf <fabian@toro-labs.com>
2016-07-15 16:32:32 -0500

    fixed filter labels and left only cvs download

--- app/admin/sales_detail_report.rb
+++ app/admin/sales_detail_report.rb
@@ -4,7 +4,7 @@ ActiveAdmin.register Order, as: 'SalesDetailReport' do
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #permit_params :id, :client_id, :client_name, :te_order_id, :event_name, :event_home_team, :event_away_team, :event_date, :event_location, :ticket_section, :ticket_row, :ticket_seats, :ticket_format, :total, :cost, :order_data, :ticket_data, :event_data, :home_team_data, :away_team_data, :refund_status, :create_date, :modified_date
 
-index do
+index :download_links => [:csv] do
     selectable_column
     column :te_order_id
     column ("Order Date")  { |order| order.create_date }
@@ -20,8 +20,8 @@ index do
     column :sale_price_per_ticket
     column :ticket_total
 end
-filter :create_date
-filter :real_event_date
+filter :create_date, label: 'Order Date Range'
+filter :real_event_date, label: 'Event Date Range'
 
 csv do 
 	column :te_order_id
