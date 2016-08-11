3503b285d186838320ca5ee4a16ddf82dbb7521d
wencelf <fabian@toro-labs.com>
2016-08-11 18:54:35 -0500

    added events list all

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -12,7 +12,7 @@ class EventsController < ApplicationController
 		                      #               responses. Any 'Logger' instance object
 		                      #               is valid. EX: Logger.new('log/te_api.log')
 		})
-		@events = @connection.venues.list(:per_page => 100000)
+		@events = @connection.events.list(:per_page => 100000, :category_id => 21)
 		render json: @events
 	end
 	def next
