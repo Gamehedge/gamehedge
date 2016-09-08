0efdae25c8c19d7db540e2f1df7c4aad45c871e3
wencelf <fabian@toro-labs.com>
2016-09-08 10:04:41 -0500

    First push to heroic

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -102,8 +102,8 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
    :address              => "smtp.gmail.com",
    :port                 => 587,
-   :user_name            => 'fabian@toro-labs.com',
-   :password             => '80932gimnasia',
+   :user_name            => ENV['EMAIL_USER'],
+   :password             => ENV['EMAIL_PASSWORD'],
    :authentication       => "plain",
    :enable_starttls_auto => true
   }
