0d5e4c1f4858f59ff89cdc754f831b92d24a58d7
Edgar Forero <edgar@toro-labs.com>
2016-09-19 18:40:50 -0500

    unit test in develop

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,16 +1,19 @@
 class TicketsController < ActionController::Base
   def list
-  	@events = TicketEvolutionService.new({:type => "tickets", :id => request.GET["id"]}).list
+  	#@events = TicketEvolutionService.new({:type => "tickets", :id => request.GET["id"]}).list
+  	#render json: @events
+    
+    url = URI.parse("https://api.ticketevolution.com/v9/ticket_groups?event_id=1013003&lightweight=true")
+    
+    req = Net::HTTP::Get.new(url.to_s)
+    req.add_field("X-Signature", "fUFmAmPIMaoHXsrT5vHtERurnUViQN50h2n4J+u6GiI=")
+    req.add_field("X-Token", "4665b0c543845003cd0f86763bdb5d43")
+    res = Net::HTTP.new(url.host, url.port)
+    res.use_ssl = true
+    response = res.request(req)
+
+    render json: response.body
       
-    #@events.each do |event|
-    #    if event.splits.size > 0
-    #        event.amount = event.splits[event.splits.size-1].to_i
-    #    else
-    #        event.amount = 0
-    #    end
-    #end
-      
-  	render json: @events
   end
   def show
   	@event = TicketEvolutionService.new({:type => "tickets", :id => request.GET["id"]}).show
