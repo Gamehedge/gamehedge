cb91540c1e369e10d32d83d4c60776145fa92402
Edgar Forero <edgar@toro-labs.com>
2016-09-19 19:32:23 -0500

    Dynamic event page

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -3,11 +3,20 @@ class TicketsController < ActionController::Base
   	#@events = TicketEvolutionService.new({:type => "tickets", :id => request.GET["id"]}).list
   	#render json: @events
     
-    url = URI.parse("https://api.ticketevolution.com/v9/ticket_groups?event_id=1013003&lightweight=true")
+    ##Start API Signature generator
+    require 'base64'
+    require 'openssl'
+    secret = "g3iR2RLeuzQA9vhDGfw5hRtGMnMDsimyOfQAJ4bi"
+    request = "GET api.ticketevolution.com/v9/ticket_groups?event_id=" + params[:id] + "&lightweight=true"
+    digest = OpenSSL::Digest::Digest.new('sha256')
+    signature = Base64.encode64(OpenSSL::HMAC.digest(digest, secret, request)).chomp
+    ##End API Signature generator
+      
+    url = URI.parse("https://api.ticketevolution.com/v9/ticket_groups?event_id=" + params[:id] + "&lightweight=true")
     
     req = Net::HTTP::Get.new(url.to_s)
-    req.add_field("X-Signature", "fUFmAmPIMaoHXsrT5vHtERurnUViQN50h2n4J+u6GiI=")
-    req.add_field("X-Token", "4665b0c543845003cd0f86763bdb5d43")
+    req.add_field("X-Signature", signature.to_s)
+    req.add_field("X-Token", "5bfd4b6110681d224a8c1fa6333f375f")
     req.add_field("Accept-Encoding", "gzip")
     req.add_field("User-Agent", "tickets (gzip)")
     res = Net::HTTP.new(url.host, url.port)
