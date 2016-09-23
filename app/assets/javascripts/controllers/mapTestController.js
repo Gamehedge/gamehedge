9c31ce43c7a862beccd78b680cdd0f44b4c2a2a3
wencelf <fabian@toro-labs.com>
2016-09-23 15:44:48 -0500

    test

--- app/assets/javascripts/controllers/mapTestController.js
+++ app/assets/javascripts/controllers/mapTestController.js
@@ -1,6 +1,6 @@
 controllers = angular.module('gamehedge')
 
-controllers.controller('mapTestController', function($scope,$routeParams,dataService,apiService,$window,$filter,$http,$timeout,$location,$rootScope,Auth){
+controllers.controller('mapTestController', function($scope,$routeParams,dataService,apiService,$window,$filter,$http,$timeout,$location,$rootScope,Auth,angularLoad){
 
     $scope.prev_filter = true;
     $scope.mob_price = 0;
