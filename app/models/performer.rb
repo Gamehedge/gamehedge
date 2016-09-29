ffeb05d4ff649c16ca7648763553b0b682d768fa
Edgar Forero G <edgar@toro-labs.com>
2016-09-29 14:29:41 -0500

    async adjust

--- app/models/performer.rb
+++ app/models/performer.rb
@@ -8,7 +8,7 @@ class Performer < ActiveRecord::Base
   has_many :home_events, class_name: "Event", foreign_key: "home_performer_id"
   has_many :away_events, class_name: "Event", foreign_key: "away_performer_id"
 	has_many :tiles
-	has_attached_file :image, :styles => { :medium => ["300x300>", :jpg], :thumb => ["100x100#", :jpg] }, :convert_options => { :all => '-background white -flatten +matte' }
+	has_attached_file :image, styles: { medium: ["150x150>", :jpg], thumb: ["100x100#", :jpg] }, convert_options: { medium: "-quality 60 -strip", :all => '-background white -flatten +matte' }
   	validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 	def display_name
 		return self.name
