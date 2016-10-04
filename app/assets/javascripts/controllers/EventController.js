c8e4243c5b1ffae6c51aa36d8bf4208f19bae596
Fabian Santos <fabian@toro-labs.com>
2016-10-04 17:16:54 -0500

    events new render test without filters

--- app/assets/javascripts/controllers/EventController.js
+++ app/assets/javascripts/controllers/EventController.js
@@ -287,11 +287,11 @@ controllers.controller('EventController', function($scope,$routeParams,dataServi
             method: 'GET',
             url: '/tickets/list/?id='+$routeParams.eventId,
         }).then(function successCallback(response) {
-        	$scope.tickets = response;
+        	$scope.tickets = response.data.ticket_groups;
         	$scope.loading = false;
         	console.log($scope.tickets);
         	var sections = [];
-        	angular.forEach($scope.tickets.data.ticket_groups , function(value, key) {
+        	angular.forEach($scope.tickets , function(value, key) {
                 value.amount = value.splits[value.splits.length-1];
         		if(sections.indexOf(value.section) == -1){
 	                sections.push(value.section);
@@ -299,24 +299,27 @@ controllers.controller('EventController', function($scope,$routeParams,dataServi
 	            }	
         	});
             var htm = "";
-            for(i=0;i<$scope.tickets.length;i++){
-                var list = $scope.tickets;
-                var select_list = ""
-                for(j=0;j<list[i].splits;j++){
-                    select_list += '<option value="'+list[i].splits[j]+'">'+list[i].splits[j]+'</option>'
+            var list = $scope.tickets;
+            var select_list = "";
+            for(i=0;i<list.length;i++){
+                select_list = "";
+                var amount = list[i].splits.reverse();
+                for(j=0;j<amount.length;j++){
+                    select_list += '<option value="'+amount[j]+'">'+amount[j]+'</option>'
                 }
-                if(list[i].public_notes != ""){
-                    htm +='<div class="row" data-section="'+list[i].section+'"><div class="hidden-xs hidden-sm col-xs-1 info-ico vertical-center full-height"><span aria-hidden="true" data-toggle="tooltip" data-placement="right" title='+list[i].public_notes+'><img src="'+info_url+'" alt="info" /></span></div><div class="col-xs-3 col-md-3 vertical-center horizontal-center section full-height">'+list[i].ssection+'</div><div class="col-xs-2 vertical-center horizontal-center full-height">'+list[i].row+'</div><div class="col-xs-3 col-md-2 vertical-center horizontal-center full-height"><div class="select-container custom-select"><select value="list[i].amount">+'+select_list+'</select></div></div><div class="col-xs-4 vertical-center horizontal-center buy-btn full-height"><span class="buy-btn-span"><button>$'+list[i]retail_price+'/ea</button><!--button class="hidden-md hidden-lg hidden-xl">$'+list[i].retail_price+'}}/ea</button--><p class="format">'+list[i].format+'</span></div></div>'
+                console.log(list[i].public_notes);
+                if(list[i].public_notes == null){
+                    htm +='<div class="row" data-section="'+list[i].section+'"><div class="hidden-xs hidden-sm col-xs-1 info-ico vertical-center full-height"></div><div class="col-xs-3 col-md-3 vertical-center horizontal-center section full-height">'+list[i].section+'</div><div class="col-xs-2 vertical-center horizontal-center full-height">'+list[i].row+'</div><div class="col-xs-3 col-md-2 vertical-center horizontal-center full-height"><div class="select-container custom-select"><select value="list[i].amount">+'+select_list+'</select></div></div><div class="col-xs-4 vertical-center horizontal-center buy-btn full-height"><span class="buy-btn-span"><button>$'+list[i].retail_price+'/ea</button><!--button class="hidden-md hidden-lg hidden-xl">$'+list[i].retail_price+'}}/ea</button--><p class="format">'+list[i].format+'</span></div></div>'
                 }
                 else{
-                    htm +='<div class="row" data-section="'+list[i].section+'"><div class="hidden-xs hidden-sm col-xs-1 info-ico vertical-center full-height"></div><div class="col-xs-3 col-md-3 vertical-center horizontal-center section full-height">'+list[i].ssection+'</div><div class="col-xs-2 vertical-center horizontal-center full-height">'+list[i].row+'</div><div class="col-xs-3 col-md-2 vertical-center horizontal-center full-height"><div class="select-container custom-select"><select value="list[i].amount">+'+select_list+'</select></div></div><div class="col-xs-4 vertical-center horizontal-center buy-btn full-height"><span class="buy-btn-span"><button>$'+list[i]retail_price+'/ea</button><!--button class="hidden-md hidden-lg hidden-xl">$'+list[i].retail_price+'}}/ea</button--><p class="format">'+list[i].format+'</span></div></div>'
+                    htm +='<div class="row" data-section="'+list[i].section+'"><div class="hidden-xs hidden-sm col-xs-1 info-ico vertical-center full-height"><span aria-hidden="true" data-toggle="tooltip" data-placement="right" title='+list[i].public_notes+'><img src="'+info_url+'" alt="info" /></span></div><div class="col-xs-3 col-md-3 vertical-center horizontal-center section full-height">'+list[i].section+'</div><div class="col-xs-2 vertical-center horizontal-center full-height">'+list[i].row+'</div><div class="col-xs-3 col-md-2 vertical-center horizontal-center full-height"><div class="select-container custom-select"><select value="list[i].amount">+'+select_list+'</select></div></div><div class="col-xs-4 vertical-center horizontal-center buy-btn full-height"><span class="buy-btn-span"><button>$'+list[i].retail_price+'/ea</button><!--button class="hidden-md hidden-lg hidden-xl">$'+list[i].retail_price+'}}/ea</button--><p class="format">'+list[i].format+'</span></div></div>'
                 }
-                $('#tickets_list').html(htm);
             }
-
-
-
-        	// $.each(sections, function(value, key) {
+            $('#tickets_list').html(htm);
+            $timeout(function () {
+                $('[data-toggle="tooltip"]').tooltip();
+            }, 100);
+            // $.each(sections, function(value, key) {
 	        //     $scope.Data.push({"section":key,"price":0,"quantity":1});
 	        // });
 	        $scope.loadMap();
