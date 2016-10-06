05a6887c26bfd1fe2ab3537f57e467ff7f5fe053
Fabian Santos <fabian@toro-labs.com>
2016-10-06 11:51:00 -0500

    map test

--- public/DVM_functions.js
+++ public/DVM_functions.js
@@ -819,7 +819,7 @@ function set_map(args) {
             clickSection = function (sts, section, section_has_tix) {
                 //e = jQuery.event.fix(e);
                 //if (section_as_row(section) || panZoom.getCurrentZoom()<1) {
-                console.log(section);
+
                 if (section_as_row(section) || (this_map_params['rows_display'] === false)) {// || (this_map_params['rows_display'] && panZoom.getCurrentZoom()<1)
                     if (section_has_tix) {
                         if (sts[0].state === 0) {
@@ -3323,6 +3323,7 @@ function click_attr_row_setting(section, row) {
 function display_section_tickets(section, unset) {
     if (section && is_section_has_tix(section)) {
         section_id = bare_section(section);
+        console.log(section_id);
         //remove section from hidden SectionPass list
         if (unset) {
             //remove from the ticket listing
