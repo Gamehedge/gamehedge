fda45025508bdae35be05a046500cf56327dfc15
Fabian Santos <fabian@toro-labs.com>
2016-10-07 18:03:20 -0500

    map drag

--- public/essentiels.js
+++ public/essentiels.js
@@ -6250,25 +6250,25 @@
     c.prototype = e;
 
     function a(i, h) {
-        console.log("error trace");
-        console.log(i);
-        console.log(h);
         var g, k, j;
         if (i.pageX || i.pageY) {
+            console.log("error 1");
+            console.log(i);
             g = i.pageX;
             k = i.pageY
         } else {
+            console.log("error 2");
+            console.log(i);
+            console.log(document.body.scrollLeft);
+            console.log(document.body.scrollTop);
+            console.log(document.documentElement.scrollLeft);
+            console.log(document.documentElement.scrollTop);
             g = i.clientX + document.body.scrollLeft + document.documentElement.scrollLeft;
             k = i.clientY + document.body.scrollTop + document.documentElement.scrollTop
         }
-        console.log(g);
-        console.log(k);
         j = b(h);
-        console.log(j);
         g -= j[0];
         k -= j[1];
-        console.log(g);
-        console.log(k);
         return {
             x: g,
             y: k
