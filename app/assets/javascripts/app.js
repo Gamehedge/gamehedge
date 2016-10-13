4e851470c4cd3976625adac2f389320267f0f2fc
Fabian Santos <fabian@toro-labs.com>
2016-10-13 14:52:10 -0500

    league rails test

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -99,9 +99,9 @@ app.config([ '$routeProvider','$locationProvider','AuthProvider', 'AnalyticsProv
             templateUrl: "map-test.html",
             controller: 'mapTestController',
         })
-        .otherwise({
-            redirectTo: '/'
-        });
+        // .otherwise({
+        //     redirectTo: '/'
+        // });
       if(window.history && window.history.pushState){
           //$locationProvider.html5Mode(true); will cause an error $location in HTML5 mode requires a  tag to be present! Unless you set baseUrl tag after head tag like so: <head> <base href="/">
 
