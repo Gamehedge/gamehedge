1ff7c17a7f252b967454f9843104b33e5e922154
Fabian Santos <fabian@toro-labs.com>
2016-10-25 16:03:02 -0500

    order complete

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -5,15 +5,15 @@ class AuthController < ApplicationController
   	else
   		response = "invalid user and password"
   	end
-  	render json: {response: response, user: Client.find_by_email(params[:email]).to_json}
+  	render json: {response: response, user: Client.find_by_email(params[:email])}
   end
   def get_user
   	if client_signed_in?
-  		response "user logged"
+  		response = "user logged"
   	else
-  		render json: "not logged in"
+  		response = "not logged in"
   	end
-  	render json: {response: response, user: current_client.to_json}
+  	render json: {response: response, user: current_client}
   end
   def send_password
   	user = Client.find_by_email(params[:email])
