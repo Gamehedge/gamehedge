2100de5768637b15748d5f8183ac2cab567af8e0
Daniil Vinokurov <danyvinokurov@gmail.com>
2017-09-28 10:25:14 -0400

    govx 15

--- app/assets/javascripts/controllers/ConfirmController.js
+++ app/assets/javascripts/controllers/ConfirmController.js
@@ -43,11 +43,21 @@ controllers.controller('ConfirmController', function($scope,$rootScope,$http,Aut
 
 					mixpanel.track("Order Complete", {"OrderID": $scope.order.te_order_id, "order_total": $scope.order.total});
 
-					console.log('ht:'+$scope.order.event_name);					
-					console.dir('o:'+$scope.order);					
-					console.log('ht:'+$scope.order.event_home_team);
-					console.log('at:'+$scope.order.event_away_team);
+					//console.log('ht:'+$scope.order.event_name);					
+					//console.dir('o:'+$scope.order);					
+					//console.log('ht:'+$scope.order.event_home_team);
+					//console.log('at:'+$scope.order.event_away_team);
 					
+
+					$rootScope.tmpPerformer = '';
+					if ($scope.order.event_name.indexOf('Baltimore Ravens') !== -1){
+						$rootScope.tmpPerformer = 'ravens';						
+					}
+					if ($scope.order.event_name.indexOf('Pittsburgh Steelers') !== -1){
+						$rootScope.tmpPerformer = 'steelers';						
+					}
+
+
 
 					fbq('track', 'Purchase', {
 						value: $scope.order.total,
